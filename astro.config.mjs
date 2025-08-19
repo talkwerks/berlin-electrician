26438b7768d90f6650d460b9684eb89615a7186b
talkwerks <max@talkwerks.com>
2025-08-19 14:08:10 +0200

    perf(css): inline critical CSS with astro-critters

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import critters from 'astro-critters';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://berlin-electrician.vercel.app'
+    site: 'https://berlin-electrician.vercel.app',
+    integrations: [
+        critters({
+            preload: 'media',
+            reduceInlineStyles: false,
+            pruneSource: true,
+        }),
+    ],
 });
